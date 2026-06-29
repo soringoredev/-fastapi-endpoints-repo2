80d652cfd25f8ea7f3b7c45a05ebd6382d209ba6
Sorin <soringoredev@gmail.com>
2026-06-29 20:29:15 +0300

    Add feature for creating new course

--- courses.py
+++ courses.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
@@ -21,11 +21,15 @@ async def read_course(course_title: str):
         if course.get('title').casefold() == course_title.casefold():
             return course
 
-@app.get("/courses/bytrainer")
+@app.get("/courses/bytrainer/")
 async def read_course_by_trainer(trainer: str):
     courses_to_return = []
     for course in COURSES:
-        if course.get('trainer').casefold() == trainer.casefold():
+        if course.get("trainer").casefold() == trainer.casefold():
             courses_to_return.append(course)
 
     return courses_to_return
+
+@app.post('/courses/create_course/')
+async def create_course(new_course=Body()):
+    COURSES.append(new_course)
